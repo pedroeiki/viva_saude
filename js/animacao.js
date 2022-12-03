564d4ceb650a09d2bd04b552ea7a1895752eba4a
PEDRO EIKI SATO DE MIRANDA <pedro.esmiranda@senacsp.edu.br>
2022-12-03 11:29:22 -0300

    :tada:Add: banner rotativo

--- js/animacao.js
+++ js/animacao.js
@@ -11,3 +11,14 @@ document.querySelector(".fecharMenu").onclick = function() {
 document.querySelector(".fechar").onclick = function() {
     document.documentElement.classList.remove("menuAtivo");
 }
+
+/*BANNER*/
+
+$(document).ready(function(){
+    $('.banner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+});
